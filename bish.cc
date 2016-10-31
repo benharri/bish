2d529ada0ecfb353334e00e841f8607edc381688
Ben Harris <mail@benharris.ch>
2016-10-30 20:21:39 -0400

    update readme

--- bish.cc
+++ bish.cc
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <string.h>
+#include <unistd.h>
 using namespace std;
+
+char** parse_line(char* line) {
+  char** cmd_w_args;
+  int cnt = 0;
+  char *pch = strtok (line," ");
+  while (pch != NULL) {
+    cmd_w_args[cnt] = pch;
+    pch = strtok (NULL, " "); cnt++;
+  }
+  return cmd_w_args;
+}
 
 int main(int argc, char **argv){
   string line;
-  cout << getenv("PS1");
+
+  cout << "bish$ ";
   while (getline(cin, line)) {
     cout << line << endl;
-    cout << getenv("PS1");
+    execv(line, parse_line(line.c_str()), NULL);
+    cout << "bish$ ";
   }
   return 0;
 }
